ab36e7d0d5b63c7b4e2927243370c64217be45b4
KOCMOHABT <KOCMODECAHTHUK@inbox.ru>
2022-03-02 12:52:45 +0500

    1

---
+++ Main/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var n = 123;
+const bigInt = 1234567890123456789012345678901234567890n;
+var str = "World";
+var age = null;
+let id = Symbol();
+var user = { name: "Вася" };
+function init() {
+    typeof 0 // "number"
+    
+    typeof 1n // "bigint"
+    
+    typeof true // "boolean"
+    
+    typeof "foo" // "string"
+    
+    typeof Symbol() // "symbol"
+    
+    typeof {} // "object"
+}
